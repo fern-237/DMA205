e4467e10dd17e7942fe8b0254217a15207906dec
Fernando <fernarias@gmail.com>
2026-06-20 13:40:58 -0700

    version 3.4

---
+++ JavaScript/menu.js
@@ -0,0 +1,20 @@
+const currentPage = window.location.pathname.split("/").pop();
+
+const menuLinks = document.querySelectorAll(
+    "header nav ul li a"
+   <nav>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>
+</nav>
+);
+
+menuLinks.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage) {
+        link.classList.add("active");
+    }
+});
